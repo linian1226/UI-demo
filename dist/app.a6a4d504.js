c66f35c81eeeeb122b85f66aab4a4b216ec03392
linian1226 <3038055649@qq.com>
2019-02-19 19:17:54 +0800

    添加loading

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12310,7 +12310,6 @@ exports.default = void 0;
 //
 //
 //
-//
 var _default = {
   props: {
     icon: {},
@@ -12344,13 +12343,14 @@ exports.default = _default;
     },
     [
       _vm.icon
-        ? _c("svg", { staticClass: "icon" }, [
-            _c("use", { attrs: { "xlink:href": "#i-" + _vm.icon } })
-          ])
+        ? _c("g-icon", { staticClass: "icon", attrs: { name: _vm.icon } })
         : _vm._e(),
       _vm._v(" "),
+      _c("g-icon", { staticClass: "loading", attrs: { name: "loading" } }),
+      _vm._v(" "),
       _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
-    ]
+    ],
+    1
   )
   var _obj
 }
@@ -12387,6 +12387,71 @@ render._withStripped = true
       
       }
     })();
+},{"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js","vue-hot-reload-api":"node_modules/vue-hot-reload-api/dist/index.js","vue":"node_modules/vue/dist/vue.common.js"}],"src/icon.vue":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+//
+//
+//
+//
+//
+var _default = {
+  props: ['name']
+};
+exports.default = _default;
+        var $16a9cc = exports.default || module.exports;
+      
+      if (typeof $16a9cc === 'function') {
+        $16a9cc = $16a9cc.options;
+      }
+    
+        /* template */
+        Object.assign($16a9cc, (function () {
+          var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("svg", { staticClass: "g-icon" }, [
+    _c("use", { attrs: { "xlink:href": "#i-" + _vm.name } })
+  ])
+}
+var staticRenderFns = []
+render._withStripped = true
+
+          return {
+            render: render,
+            staticRenderFns: staticRenderFns,
+            _compiled: true,
+            _scopeId: null,
+            functional: undefined
+          };
+        })());
+      
+    /* hot reload */
+    (function () {
+      if (module.hot) {
+        var api = require('vue-hot-reload-api');
+        api.install(require('vue'));
+        if (api.compatible) {
+          module.hot.accept();
+          if (!module.hot.data) {
+            api.createRecord('$16a9cc', $16a9cc);
+          } else {
+            api.reload('$16a9cc', $16a9cc);
+          }
+        }
+
+        
+        var reloadCSS = require('_css_loader');
+        module.hot.dispose(reloadCSS);
+        module.hot.accept(reloadCSS);
+      
+      }
+    })();
 },{"_css_loader":"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js","vue-hot-reload-api":"node_modules/vue-hot-reload-api/dist/index.js","vue":"node_modules/vue/dist/vue.common.js"}],"src/app.js":[function(require,module,exports) {
 "use strict";
 
@@ -12394,14 +12459,18 @@ var _vue = _interopRequireDefault(require("vue"));
 
 var _button = _interopRequireDefault(require("./button"));
 
+var _icon = _interopRequireDefault(require("./icon"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 _vue.default.component('g-button', _button.default);
 
+_vue.default.component('g-icon', _icon.default);
+
 new _vue.default({
   el: '#app'
 });
-},{"vue":"node_modules/vue/dist/vue.common.js","./button":"src/button.vue"}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"vue":"node_modules/vue/dist/vue.common.js","./button":"src/button.vue","./icon":"src/icon.vue"}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,9 @@
 <template>
     <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
-        <svg v-if="icon" class="icon">
-            <use :xlink:href="`#i-${icon}`"></use>
-        </svg>
+        <g-icon v-if="icon" :name="icon" class="icon"></g-icon>
+        <g-icon name="loading" class="loading"></g-icon>
         <div class="content">
-            <slot></slot>
+            <slot/>
         </div>
     </button>
 </template>
@@ -23,6 +22,10 @@ export default {
 }
 </script>
 <style lang="scss">
+    @keyframes spin {
+        0% {transform: rotate(0deg);}
+        100% {transform:rotate(360deg);}
+    }
     .g-button{
         font-size: var(--font-size);
         height: var(--button-height);
@@ -30,6 +33,7 @@ export default {
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        vertical-align: middle;
         display: inline-flex;justify-content:center; align-items:center;
         &:hover {
             border-color: var(--border-color-hover);
@@ -45,6 +49,9 @@ export default {
         &.icon-right{
             > .content{order:1;}
             > .icon{order:2;margin-left:.1em;}
+        }
+        .loading{
+            animation:spin 2s infinite linear;
         }
     }
 </style>
