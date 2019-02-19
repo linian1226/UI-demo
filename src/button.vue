07dad10336d67a277ddc8999532c13a623a079a2
linian1226 <3038055649@qq.com>
2019-02-19 15:53:47 +0800

    添加icon和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,25 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
 export default {
-    
+    props:{
+        icon:{},
+        iconPosition: {
+            type:String,
+            default:'left',
+            validator(value){
+                return value === 'left' || value === 'right'
+            }
+        }
+    }
 }
 </script>
 <style lang="scss">
@@ -14,6 +30,7 @@ export default {
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display: inline-flex;justify-content:center; align-items:center;
         &:hover {
             border-color: var(--border-color-hover);
         }
@@ -22,6 +39,12 @@ export default {
         }
         &:focus {
             outline: none;
+        }
+        > .content{order:2;}
+        > .icon{order:1;margin-right:.1em;}
+        &.icon-right{
+            > .content{order:1;}
+            > .icon{order:2;margin-left:.1em;}
         }
     }
 </style>
